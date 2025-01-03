c0987b4cc6b53a24603327fb6b935735abcee248
Wei Da <wd312@gpu-q-13.data.cluster>
2025-01-03 16:54:10 +0000

    add the evaluation scripts for roberta model

--- src/sample.py
+++ src/sample.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     data = utils.jload("./data/sharegpt_v3_full.jsonl")
     
     # random sample 20k
-    N = 20000
+    N = 50000
     data_mask = np.random.choice(len(data), N, replace=False)
     data = [data[i] for i in data_mask]
-    data_train = data[:10000]
+    data_train = data[:40000]
     data_train = utils.jsort(data_train, key="id", integer=True)
     data_val = data[10000:]
     data_val = utils.jsort(data_val, key="id", integer=True)
 
     # save to json
-    utils.jdump(data_train, "./data/sharegpt-train-10k.json")
+    utils.jdump(data_train, "./data/sharegpt-train-40k.json")
     utils.jdump(data_val, "./data/sharegpt-val-10k.json")
--- src/train_roberta.py
+++ src/train_roberta.py
@@ -14,11 +14,11 @@ os.environ["WANDB_DISABLED"] = "true"
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--regression-model-name", type=str, default="roberta-base")
-    parser.add_argument("--tokenizer", type=str, default="meta-llama/Llama-2-7b-hf")
-    parser.add_argument("--train-data-path", type=str, default="data/sharegpt-train-10k.json")
+    parser.add_argument("--tokenizer", type=str, default="./ckpts/vicuna-7b")
+    parser.add_argument("--train-data-path", type=str, default="data/sharegpt-train-40k.json")
     parser.add_argument("--val-data-path", type=str, default="data/sharegpt-val-10k.json")
     parser.add_argument("--output-dir", type=str, default="./ckpts/roberta-length-prediction")
-    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--epochs", type=int, default=2)
     parser.add_argument("--num-eval-examples", type=int, default=1000)
     args = parser.parse_args()
     return args
@@ -43,15 +43,28 @@ if __name__ == "__main__":
     args = parse_args()
     train_data = utils.jload(args.train_data_path)
     val_data = utils.jload(args.val_data_path)
-    tokenizer_model= AutoTokenizer.from_pretrained(args.tokenizer)
+    tokenizer_model= AutoTokenizer.from_pretrained(args.tokenizer, local_files_only=True)
 
     train_data, _ = generate_regression_dataframe(tokenizer_model, train_data)
     num_eval_examples = args.num_eval_examples
-    val_data, _ = generate_regression_dataframe(tokenizer_model, val_data, num_eval_examples)
+    val_data, val_data_list = generate_regression_dataframe(tokenizer_model, val_data, num_eval_examples)
 
     model_args = ClassificationArgs()
     model_args.num_train_epochs = args.epochs
     model_args.regression = True
+    model_args.overwrite_output_dir = True
+    model_args.save_steps = -1
+    model_args.save_model_every_epoch = False
+    model_args.save_model_every_epoch = False
+    model_args.output_dir = args.output_dir
+
+    model_args.learning_rate = 1e-5
+    # model_args.warmup_ratio = 0.03
+    # model_args.scheduler = "cosine_schedule_with_warmup"
+
+    model_args.use_early_stopping = True
+    model_args.early_stopping_delta = 0.01
+    model_args.evaluate_during_training_steps = 1000
 
     model = ClassificationModel(
     "roberta",
@@ -59,15 +72,16 @@ if __name__ == "__main__":
     num_labels=1,
     args=model_args)
 
-    model.train_model(train_data, output_dir=args.output_dir)
+    model.train_model(train_data)
     result, model_outputs, wrong_predictions = model.eval_model(val_data)
+    model.save_model()
     print(f"evaluation result: {result}")
 
     d_max = []
     assert len(val_data_list) == len(model_outputs)
     for i in range(len(val_data_list)):
         x = val_data_list[i][1]
-        y = float(model_outputs(i))
+        y = float(model_outputs[i])
         d_max.append(abs(x - y))
 
     diff = sum(d_max)
