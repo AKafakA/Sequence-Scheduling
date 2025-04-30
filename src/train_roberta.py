dd5115fe81096fcae555183ca7e0e8aee31fb71a
AKafakA <daweiuestc@gmail.com>
2025-04-30 14:14:40 +0100

    fine tune the train_roberta config

--- src/train_roberta.py
+++ src/train_roberta.py
@@ -13,11 +13,11 @@ os.environ["WANDB_DISABLED"] = "true"
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--regression-model-name", type=str, default="roberta-large")
+    parser.add_argument("--regression-model-name", type=str, default="roberta-base")
     parser.add_argument("--tokenizer", type=str, default="meta-llama/Llama-2-7b-hf")
     parser.add_argument("--train-data-path", type=str, default="data/sharegpt-train-40k.json")
     parser.add_argument("--val-data-path", type=str, default="data/sharegpt-val-10k.json")
-    parser.add_argument("--output-dir", type=str, default="./ckpts/roberta-length-prediction/large")
+    parser.add_argument("--output-dir", type=str, default="./ckpts/roberta-length-prediction/base")
     parser.add_argument("--epochs", type=int, default=200)
     parser.add_argument("--num-eval-examples", type=int, default=1000)
     parser.add_argument("--reload-model", type=bool, default=False)
@@ -52,22 +52,19 @@ if __name__ == "__main__":
 
     model_args = ClassificationArgs()
     model_args.num_train_epochs = args.epochs
-    model_args.train_batch_size = 32
     model_args.regression = True
     model_args.overwrite_output_dir = True
-    model_args.save_steps = -1
     model_args.save_model_every_epoch = False
     model_args.save_model_every_epoch = False
     model_args.output_dir = args.output_dir
-    model_args.evaluate_during_training = True
-
+    model_args.save_steps = -1
     model_args.learning_rate = 1e-5
-    # model_args.warmup_ratio = 0.03
-    model_args.scheduler = "cosine_schedule_with_warmup"
+
+    model_args.warmup_ratio = 0.03
+    model_args.scheduler = "polynomial_decay_schedule_with_warmup"
 
     model_args.use_early_stopping = True
     model_args.early_stopping_delta = 10
-    model_args.evaluate_during_training_steps = 5000
     model_args.early_stopping_patience = 10
 
     if args.reload_model:
@@ -79,27 +76,9 @@ if __name__ == "__main__":
                 num_labels=1,
                 args=model_args)
 
-    model.train_model(train_df=train_data, eval_df=val_data)
-    result, model_outputs, wrong_predictions = model.eval_model(val_data)
+    model.train_model(train_df=train_data)
     model.save_model()
-    print(f"evaluation result: {result}")
-
-    d_max = []
-    assert len(val_data_list) == len(model_outputs)
-    for i in range(len(val_data_list)):
-        x = val_data_list[i][1]
-        y = float(model_outputs[i])
-        d_max.append(abs(x - y))
-
-    diff = sum(d_max)
-    acc_50t = sum([1 if x <= 50 else 0 for x in d_max])
-    acc_100t = sum([1 if x <= 100 else 0 for x in d_max])
-
-    print(f"# Samples: {num_eval_examples}")
-    print(f"Error: {diff / num_eval_examples}")
-    print(f"Acc-50: {acc_50t / num_eval_examples}")
-    print(f"Acc-100: {acc_100t / num_eval_examples}")
-
+    print("train finished")
 
     
 
